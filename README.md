4bb8076b0010c80ff413b573680eb61b33916a88
Clickism <yagizaktasx@gmail.com>
2025-07-07 19:32:39 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+I found it a bit annoying that we cannot use constants (.equ directives) to make our assembly more readable/debuggable. So I created a python script and a *pseudo*-language called **spp** (s++) that allows .equ directives and compiles into **.s** files.
+
+To use this, simply create a **.spp** file with .equ directives and run the command
+`python3 spp.py programs/public/os/yourprogram.spp`
+which will output a file `yourprogram.s` with all the constants replaced.
+
+Example .spp file:
+```asm
+.equ MAGIC_NUMBER = 0x100
+.equ TEST = 400
+_start:
+    li t0, MAGIC_NUMBER
+    li t1, TEST
+    mul t0, t0, t1
+```
+It will compile into:
+```asm
+_start:
+    li t0, 0x100
+    li t1, 400
+    mul t0, t0, t1
+```
+It might be important to compile and remove the .spp files before submitting.
+
+**The software is provided "as is" and there is no guarantee that it will work as intended and any possible problems caused by this is your responsibility. Use at your own risk!**
---
+++ spp.py
@@ -0,0 +1,81 @@
+# Written by: Yagiz Aktas
+
+# MIT License
+#
+# Copyright (c) 2025 Yagiz Aktas
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+# and associated documentation files (the "Software"), to deal in the Software without restriction, 
+# including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+# sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or 
+# substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+# BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import sys
+
+def parse_spp_file(input_file: str) -> list[str]:
+    file = open(input_file, 'r')
+    lines = file.readlines()
+    constants = []
+    output_lines = []
+    for line in lines:
+        if line.startswith('.equ'):
+            parts = line.split()
+            if len(parts) == 4:
+                name = parts[1]
+                value = parts[3]
+                for (constant_name, constant_value) in constants:
+                    if constant_name == name:
+                        print(f"Error: Constant {name} already defined as {constant_value}")
+                        sys.exit(1)
+                    else:
+                        value = value.replace(constant_name, constant_value)
+                constants.append((name, value))
+                print(f"Added constant: {name} = {value}")
+            else:
+                print(f"Error: Invalid .equ constant declaration: {line.strip()}")
+                sys.exit(1)
+        else:
+            if '#' in line:
+                code, comment = line.split('#', 1)
+                for (name, value) in constants:
+                    code = code.replace(name, value)
+                line = code + '#' + comment
+            else:
+                for (name, value) in constants:
+                    line = line.replace(name, value)
+            output_lines.append(line)
+
+    return output_lines
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python spp.py <input_file> [output_file]")
+        sys.exit(1)
+    input_file = sys.argv[1]
+    if not input_file.endswith(".spp"):
+        print("Input file does not end with .spp")
+        sys.exit(1)
+    output_file = sys.argv[2] if len(sys.argv) > 2 else input_file.rstrip('.spp') + '.s'
+    output_lines = parse_spp_file(input_file)
+    with open(output_file, 'w') as out_file:
+        for line in output_lines:
+            out_file.write(line)
+    print(f"Output written to {output_file}")
+    pass
+
+version = "0.1"
+
+if __name__ == "__main__":
+    print("-----------------")
+    print(f"SPP Compiler {version}")
+    print("-----------------")
+    main()
